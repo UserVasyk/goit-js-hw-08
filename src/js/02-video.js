f2e0f654d53a5ec85da3672273744dcc380a9ec8
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2022-11-07 14:35:48 +0200

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,14 @@
-import vimeo from "@vimeo/player";
+import vimeo from '@vimeo/player';
 
-import throttle from "lodash.throttle";
-const iframe = document.querySelector("#vimeo-player");
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
 
 const player = new vimeo(iframe);
 
-player.on("timeupdate", throttle(onIframeVideo, 1000));
+player.on('timeupdate', throttle(onIframeVideo, 1000));
 
 function onIframeVideo(evt) {
-  localStorage.setItem("videoplayer-current-time", evt.seconds);
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
 }
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
